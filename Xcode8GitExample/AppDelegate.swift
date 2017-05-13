1d9aa4a880ee968ff6d7600181e5a25f11a559d8
Code Element MacBook <element@code-element-macbook.local>
2017-05-13 17:17:35 -0400

    Initial Commit

---
+++ Xcode8GitExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Xcode8GitExample
+//
+//  Created by Code Element MacBook on 5/13/17.
+//  Copyright © 2017 Albee's Apps. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Xcode8GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8GitExample
+//
+//  Created by Code Element MacBook on 5/13/17.
+//  Copyright © 2017 Albee's Apps. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
